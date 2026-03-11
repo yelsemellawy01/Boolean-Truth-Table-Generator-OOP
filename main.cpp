9e4bb065aedc9d247d4973e719764635d20634a2
Youssuf <yelsemellawy01@qub.ac.uk>
2026-03-11 10:20:43 +0000

    Added NOT operator and added explaining operation functionality to code

--- main.cpp
+++ main.cpp
@@ -15,6 +15,34 @@ int OR_Location = input.find("OR");
 int NAND_Location = input.find("NAND"); 
 int XOR_Location = input.find("XOR"); 
 int NOR_Location = input.find("NOR");
+int NOT_Location = input.find("NOT");
+
+
+
+std::cout << "What the operators actually mean: " << std::endl; 
+if(AND_Location != std::string::npos){ 
+    std::cout << "AND: True only if both inputs are true." << std::endl; 
+} 
+if(OR_Location != std::string::npos){
+    std::cout << "OR: True when 1 or more inputs are true." << std::endl; 
+}
+if(NAND_Location != std::string::npos){ 
+    std::cout << "NAND: False only if both inputs are true." << std::endl;
+}
+if(XOR_Location != std::string::npos){ 
+    std::cout << "XOR: True when only 1 input is true." << std::endl;
+}
+if(NOR_Location != std::string::npos){ 
+    std::cout << "NOR: False when 1 or more inputs are true." << std::endl;
+}
+if(NOT_Location != std::string::npos){ 
+    std::cout << "NOT: Inverts the input." << std::endl;
+}
+
+
+
+
+
 
 
 
